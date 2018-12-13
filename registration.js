fca1a79602f557876a8f171f08afe2f8ffe7fd8c
sunithasu <45839050+sunithasu@users.noreply.github.com>
2018-12-13 13:39:57 +0530

    Create registration.js

---
+++ registration.js
@@ -0,0 +1,36 @@
+Register validation.js
+x=document.getElement by Id("email")value:
+if(x=="){
+alert("email must be filled");
+return false;
+}
+x=document.get element ById("password")value;
+if(x=="){
+alert("password must be filled");
+return false;
+}
+x=document.get element ById("user name")value;
+if(x=="){
+alert("user name must be filled");
+return false;
+}
+x=document.get element ById("college name")value;
+if(x=="){
+alert("college name must be filled");
+return false;    
+}
+x=document.get element ById("degree")value;
+if(x=="){
+alert("degree must be filled");
+return false;    
+}
+x=document.get element ById("branch")value;
+if(x=="){
+alert("branch must be filled");
+return false;   
+}
+x=document.get element ById("register number")value;
+if(x=="){
+alert("register number must be filled");
+return false;    
+}
