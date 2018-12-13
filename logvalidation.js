9cb94105099aaf5e99f30b0bad0f10c2fb0cd50c
sunithasu <45839050+sunithasu@users.noreply.github.com>
2018-12-13 13:36:01 +0530

    Create logvalidation.js

---
+++ logvalidation.js
@@ -0,0 +1,12 @@
+log validation.js
+x=document.getElement by Id("email").value:
+if(x=="){
+alert("email must be filled");
+return false;
+}
+x=document.get element ById("password")value;
+if(x=="){
+alert("password must be filled");
+return false
+}
+
